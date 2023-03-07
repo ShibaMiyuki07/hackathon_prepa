7415c45932dc44523cd7cdb10ea09b5ac64c89a9
ShibaMiyuki07 <94074919+ShibaMiyuki07@users.noreply.github.com>
2023-03-07 08:31:55 +0300

    Create base.sql

---
+++ base.sql
@@ -0,0 +1,88 @@
+create sequence idEtudiant;
+
+create sequence idAnneeEtude;
+
+create sequence idCycle;
+
+create sequence idMatiere;
+
+create sequence idJour;
+
+create table matiere
+(
+    idMatiere char(30) primary key default concat('M00',nextVal('idMatiere')),
+    nomMatiere char(255)
+);
+
+create table anneeEtude
+(
+    idAnneeEtude char(30) primary key default concat('A00',nextVal('idAnneeEtude')),
+    annee char(255)
+);
+
+create table etudiant
+(
+    idetudiant char(30) primary key default concat('E00',nextVal('idEtudiant')),
+    nomEtudiant char(255),
+    prenomEtudiant char(255),
+    dateNaissance date,
+    email char(255),
+    mdp char(255),
+    idAnneeEtude char(30),
+    photoprofil text,
+    foreign key(idAnneeEtude) references anneeEtude(idAnneeEtude)
+);
+
+create table cycle
+(
+    idCycle char(30) primary key default concat('C00',nextVal('idCycle')),
+    idEtudiant char(30),
+    duree int,
+    foreign key(idetudiant) references etudiant(idetudiant)
+);
+
+create table jour
+(
+    idJour char(30) primary key default concat('J00',nextVal('idJour')),
+    nomJour char(255)
+);
+
+create table note_etudiant_matiere
+(
+    idetudiant char(30),
+    idMatiere char(30),
+    note float,
+    foreign key(idetudiant) references etudiant(idetudiant),
+    foreign key(idMatiere) references matiere(idMatiere)
+);
+
+create table emploi_temps_manuelle
+(
+    idetudiant char(30),
+    idJour char(30),
+    heureDebut time,
+    heureFin time,
+    idMatiere char(30),
+    etat int default 0,
+    check (heureDebut<heureFin),
+    check (heureFin>heureDebut),
+    foreign key(idetudiant) references etudiant(idetudiant),
+    foreign key (idJour) references jour(idJour),
+    foreign key(idMatiere) references matiere(idMatiere)
+);
+
+
+create table emploi_temps_auto
+(
+    idetudiant char(30),
+    idJour char(30),
+    heureDebut time,
+    heureFin time,
+    idMatiere char(30),
+    etat int default 0,
+    check (heureDebut<heureFin),
+    check (heureFin>heureDebut),
+    foreign key(idetudiant) references etudiant(idetudiant),
+    foreign key (idJour) references jour(idJour),
+    foreign key(idMatiere) references matiere(idMatiere)
+);
