945164682062ac5ed82e5aa5d0ca7a39c0c164b6
ShibaMiyuki07 <94074919+ShibaMiyuki07@users.noreply.github.com>
2023-03-07 19:46:08 +0300

    CRUD

--- WebService/demo/src/main/java/com/main/Model/Cycle.java
+++ WebService/demo/src/main/java/com/main/Model/Cycle.java
@@ -5,6 +5,13 @@
  */
 package com.main.Model;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import com.main.Base.Connexion;
 
 /**
  *
@@ -42,5 +49,108 @@ public class Cycle {
     public void setDuree(Integer duree) {
         this.duree = duree;
     }
+    
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    //CRUD
+    public void create(Cycle cycle) throws SQLException
+	{
+		String requete = "insert into cycle values(default,'"+cycle.getIdEtudiant()+"','"+cycle.getDuree()+"')";
+		Connection connect = null;
+		Statement state = null;
+		
+		try
+		{
+			connect = new Connexion().setConnect();
+			state = connect.createStatement();
+			state.execute(requete);
+		}
+		catch(Exception e)
+		{
+			
+		}
+		finally
+		{
+			if(connect != null) connect.close();
+			if(state != null) state.close();
+		}
+	}
+    
+    public ArrayList<Cycle> read() throws SQLException
+	{
+		String requete = "select * from cycle";
+		ArrayList<Cycle> liste = new ArrayList<>();
+		Connection connect = null;
+		Statement state = null;
+		try
+		{
+			connect = new Connexion().setConnect();
+			state = connect.createStatement();
+			ResultSet rs = state.executeQuery(requete);
+			while(rs.next())
+			{
+				Cycle cycle = new Cycle();
+				cycle.setIdCycle(rs.getString("idcycle"));
+				cycle.setIdEtudiant(rs.getString("idetudiant"));
+				cycle.setDuree(rs.getInt("duree"));
+				liste.add(cycle);
+			}
+			rs.close();
+		}
+		catch(Exception e)
+		{
+			
+		}
+		finally
+		{
+			if(connect != null) connect.close();
+			if(state != null) state.close();
+		}
+		return liste;
+	}
+	
+	public void update(Cycle cycle,String id) throws SQLException
+	{
+		String requete = "update anneeEtude set duree='"+cycle.getDuree()+"' where idCycle='"+id+"'";
+		Connection connect = null;
+		Statement state = null;
+		try
+		{
+			connect = new Connexion().setConnect();
+			state = connect.createStatement();
+			state.execute(requete);
+		}
+		catch(Exception e)
+		{
+			
+		}
+		finally
+		{
+			if(connect != null) connect.close();
+			if(state != null) state.close();
+		}
+	}
+	
+	
+	public void delete(String id) throws SQLException
+	{
+		String requete = "delete from anneeEtude where idCycle='"+id+"'";
+		Connection connect = null;
+		Statement state = null;
+		try
+		{
+			connect = new Connexion().setConnect();
+			state = connect.createStatement();
+			state.execute(requete);
+		}
+		catch(Exception e)
+		{
+			
+		}
+		finally
+		{
+			if(connect != null) connect.close();
+			if(state != null) state.close();
+		}
+	}
 
 }
--- WebService/demo/src/main/java/com/main/Model/Note_etudiant_matiere.java
+++ WebService/demo/src/main/java/com/main/Model/Note_etudiant_matiere.java
@@ -5,6 +5,14 @@
  */
 package com.main.Model;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import com.main.Base.Connexion;
+
 /**
  *
  * @author RickyBic
@@ -12,7 +20,7 @@ package com.main.Model;
 public class Note_etudiant_matiere {
 
     private String idetudiant;
-    private String idMatiere;
+    private String idmatiere;
     private Float note;
 
     
@@ -26,20 +34,129 @@ public class Note_etudiant_matiere {
         this.idetudiant = idetudiant;
     }
 
-    public String getIdMatiere() {
-        return idMatiere;
-    }
+    
 
-    public void setIdMatiere(String idMatiere) {
-        this.idMatiere = idMatiere;
-    }
+    public String getIdmatiere() {
+		return idmatiere;
+	}
 
-    public Float getNote() {
+	public void setIdmatiere(String idmatiere) {
+		this.idmatiere = idmatiere;
+	}
+
+	public Float getNote() {
         return note;
     }
 
     public void setNote(Float note) {
         this.note = note;
     }
+    
+    
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    //CRUD
+    public void create(Note_etudiant_matiere note_etudiant_matiere) throws SQLException
+	{
+		String requete = "insert into note_etudiant_matiere values('"+note_etudiant_matiere.getIdetudiant()+"','"+note_etudiant_matiere.getIdmatiere()+"','"+note_etudiant_matiere.getNote()+"')";
+		Connection connect = null;
+		Statement state = null;
+		
+		try
+		{
+			connect = new Connexion().setConnect();
+			state = connect.createStatement();
+			state.execute(requete);
+		}
+		catch(Exception e)
+		{
+			
+		}
+		finally
+		{
+			if(connect != null) connect.close();
+			if(state != null) state.close();
+		}
+	}
+    
+    public ArrayList<Note_etudiant_matiere> read() throws SQLException
+	{
+		String requete = "select * from note_etudiant_matiere";
+		ArrayList<Note_etudiant_matiere> liste = new ArrayList<>();
+		Connection connect = null;
+		Statement state = null;
+		try
+		{
+			connect = new Connexion().setConnect();
+			state = connect.createStatement();
+			ResultSet rs = state.executeQuery(requete);
+			while(rs.next())
+			{
+				Note_etudiant_matiere note_etudiant_matiere = new Note_etudiant_matiere();
+				note_etudiant_matiere.setIdetudiant(rs.getString("idetudiant"));
+				note_etudiant_matiere.setIdmatiere(rs.getString("idmatiere"));
+				note_etudiant_matiere.setNote(rs.getFloat("note"));
+				liste.add(note_etudiant_matiere);
+			}
+			rs.close();
+		}
+		catch(Exception e)
+		{
+			
+		}
+		finally
+		{
+			if(connect != null) connect.close();
+			if(state != null) state.close();
+		}
+		return liste;
+	}
+	
+	public void update(Note_etudiant_matiere note_etudiant_matiere,String id) throws SQLException
+	{
+		String requete = "update note_etudiant_matiere set note='"+note_etudiant_matiere.getNote()+"' where idEtudiant='"+id+"' and idMatiere='"+note_etudiant_matiere.getIdmatiere()+"'";
+		Connection connect = null;
+		Statement state = null;
+		try
+		{
+			connect = new Connexion().setConnect();
+			state = connect.createStatement();
+			state.execute(requete);
+		}
+		catch(Exception e)
+		{
+			
+		}
+		finally
+		{
+			if(connect != null) connect.close();
+			if(state != null) state.close();
+		}
+	}
+	
+	
+	public void delete(String id) throws SQLException
+	{
+		String requete = "delete from note_etudiant_matiere where idNote_etudiant_matiere='"+id+"'";
+		Connection connect = null;
+		Statement state = null;
+		try
+		{
+			connect = new Connexion().setConnect();
+			state = connect.createStatement();
+			state.execute(requete);
+		}
+		catch(Exception e)
+		{
+			
+		}
+		finally
+		{
+			if(connect != null) connect.close();
+			if(state != null) state.close();
+		}
+	}
+
+    
+    
 
 }
